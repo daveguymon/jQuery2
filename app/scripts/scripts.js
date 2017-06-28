feeb1adf3ee0fd7c17268c6c2609557b17fd60ac
Dave Guymon <davidwguymon@gmail.com>
2017-06-28 17:02:58 -0600

    Completed instructions; jQuery not executing in index.html.

--- app/scripts/scripts.js
+++ app/scripts/scripts.js
@@ -22,6 +22,23 @@ $(document).ready(function () {
                     'linear');
             };
 
+            var advanceTask = function (task) {
+                var modified = task.innerText.trim()
+                for (var i = 0; i < listo.length; i++) {
+                    if (listo[i].task === modified) {
+                        if (listo[i].id === 'new') {
+                            listo[i].id = 'inProgress';
+                        } else if (listo[i].id === 'inProgress') {
+                            listo[i].id = 'archived';
+                        } else {
+                            listo.splice(i, 1);
+                        }
+                        break;
+                    }
+                }
+                task.remove();
+            };
+
 
             //variables above
 
@@ -51,7 +68,30 @@ $(document).ready(function () {
                 $('#newTaskForm').fadeToggle('fast', 'linear');
             });
 
+            $(document).on('click', '#item', function (e) {
+                e.preventDefault();
+                var task = this;
+                advanceTask(task);
+                this.id = 'inProgress';
+                $('#currentList').append(this.outerHTML);
+            });
+
+            $(document).on('click', '#inProgress', function (e) {
+                e.preventDefault();
+                var task = this;
+                task.id = "archived";
+                var changeIcon = task.outerHTML.replace('glyphicon-arrow-right', 'glyphicon-remove');
+                advanceTask(task);
+                $('#archivedList').append(changeIcon);
+            });
+
+            $(document).on('click', '#archived', function (e) {
+                e.preventDefault();
+                var task = this;
+                advanceTask(task);
+            });
 
 
 
-        }
+
+        });
