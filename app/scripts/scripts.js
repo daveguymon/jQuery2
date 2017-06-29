5253495797245dc39a91afc3dee40dc18284a9ae
Dave Guymon <davidwguymon@gmail.com>
2017-06-29 09:26:08 -0600

    Redid the project. Now working. Still need to write code for local storage.

--- app/scripts/scripts.js
+++ app/scripts/scripts.js
@@ -1,97 +1,89 @@
 $(document).ready(function () {
-            //    variables below
-            var listo = [];
-            var addTask = function (task) {
-                if (task) {
-                    task = new Task(task);
-                    listo.push(task);
 
-                    $('#newItemInput').val('');
-                    ('#newList').append(
-                        '<a href="#finish" class="" id="item">' +
-                        '<li class="list-group-item">' +
-                        '<h3>' + task.task + '</h3>' +
-                        '<span class="arrow pull-right">' +
-                        '<i class="glyphicon glyphicon-arrow-right">' +
-                        '</span>' +
-                        '</li>' +
-                        '</a>'
-                    );
+    $('#newTaskForm').hide();
+
+    //    STEP 4 Function
+    var advanceTask = function (task) {
+        var modified = task.innerText.trim()
+        for (var i = 0; i < listo.length; i++) {
+            if (listo[i].task === modified) {
+                if (listo[i].id === 'new') {
+                    listo[i].id = 'inProgress';
+                } else if (listo[i].id === 'inProgress') {
+                    listo[i].id = 'archived';
+                } else {
+                    listo.splice(i, 1);
                 }
-                $('#newTaskForm').slideToggle('fast'
-                    'linear');
-            };
+                break;
+            }
+        }
+        task.remove();
+    };
+    //    end STEP 4 Function
 
-            var advanceTask = function (task) {
-                var modified = task.innerText.trim()
-                for (var i = 0; i < listo.length; i++) {
-                    if (listo[i].task === modified) {
-                        if (listo[i].id === 'new') {
-                            listo[i].id = 'inProgress';
-                        } else if (listo[i].id === 'inProgress') {
-                            listo[i].id = 'archived';
-                        } else {
-                            listo.splice(i, 1);
-                        }
-                        break;
-                    }
-                }
-                task.remove();
-            };
+    var listo = [];
+    var Task = function (task) {
+        this.task = task;
+        this.id = 'new';
+    }
+    var addTask = function (task) {
+        if (task) {
+            task = new Task(task);
+            listo.push(task);
 
+            $('#newItemInput').val('');
+            $('#newList').append(
+                '<a href="#finish" class="" id="item">' +
+                '<li class="list-group-item">' +
+                '<h3>' + task.task + '</h3>' +
+                '<span class="arrow pull-right">' +
+                '<i class="glyphicon glyphicon-arrow-right">' +
+                '</span>' +
+                '</li>' +
+                '</a>'
+            );
+        }
 
-            //variables above
+        $('#newTaskForm').slideToggle('fast', 'linear');
+    };
 
-            $('#newTaskForm').hide();
+    $('#saveNewItem').on('click', function (e) {
+        e.preventDefault();
+        var task = $('#newItemInput').val().trim();
+        addTask(task);
+    })
 
-            var Task = function (task) {
-                this.task = task;
-                this.id = 'new';
-            };
-            //    new task constructor
+    $('#add-todo').on('click', function () {
+        $('#newTaskForm').fadeToggle('fast', 'linear');
+    });
 
-            $('#saveNewItem').on('click', function (e) {
-                e.preventDefault();
-                var task = $('#newItemInput').val().trim();
-                addTask(task);
-            });
+    $('#cancel').on('click', function (e) {
+        e.preventDefault();
+        $('#newTaskForm').fadeToggle('fast', 'linear');
+    });
 
+    $(document).on('click', '#item', function (e) {
+        e.preventDefault();
+        var task = this;
+        advanceTask(task);
+        this.id = 'inProgress';
+        $('#currentList').append(this.outerHTML);
+    });
 
-            //    opens form
-            $('#add-todo').on('click', function () {
-                $('#newTaskForm').fadeToggle('fast', 'linear');
-            });
+    $(document).on('click', '#inProgress', function (e) {
+        e.preventDefault();
+        var task = this;
+        task.id = "archived";
+        var changeIcon = task.outerHTML.replace('glyphicon-arrow-right', 'glyphicon-remove');
+        advanceTask(task);
+        $('#archivedList').append(changeIcon);
+    })
 
-            //    closes form
-            $('#cancel').on('click', function (e) {
-                e.preventDefault();
-                $('#newTaskForm').fadeToggle('fast', 'linear');
-            });
+    $(document).on('click', '#archived', function (e) {
+        e.preventDefault();
+        var task = this;
+        advanceTask(task);
+    });
 
-            $(document).on('click', '#item', function (e) {
-                e.preventDefault();
-                var task = this;
-                advanceTask(task);
-                this.id = 'inProgress';
-                $('#currentList').append(this.outerHTML);
-            });
-
-            $(document).on('click', '#inProgress', function (e) {
-                e.preventDefault();
-                var task = this;
-                task.id = "archived";
-                var changeIcon = task.outerHTML.replace('glyphicon-arrow-right', 'glyphicon-remove');
-                advanceTask(task);
-                $('#archivedList').append(changeIcon);
-            });
-
-            $(document).on('click', '#archived', function (e) {
-                e.preventDefault();
-                var task = this;
-                advanceTask(task);
-            });
-
-
-
-
-        });
+    //    end of jquery
+});
